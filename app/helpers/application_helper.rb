b370b1df6c21c81be7204e4d47afb29416af3584
sebastien <s.weyn@mail.utexas.edu>
2010-10-25 16:36:16 -0500

    Conversion to Longhorn Seq Database

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def title
-    base_title = "FGStream RNA-seq Database Prototype"
+    base_title = "Longhorn Sequencing Database Prototype"
     if @title.nil?
       base_title
     else
@@ -10,6 +10,6 @@ module ApplicationHelper
   end
   
   def logo
-    image_tag("logo.png", :alt => "The Functional Genomics Research Stream", :class => "round", :size => 10)
+    image_tag("longhorn-sequence.png", :alt => "The Longhorn Sequencing Database", :class => "round", :size => 10, :height => "15%")
   end
 end
