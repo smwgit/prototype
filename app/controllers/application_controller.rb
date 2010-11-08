42910736952c6e05497b24d5b1612a256c53a6db
sebastien <s.weyn@mail.utexas.edu>
2010-11-08 16:26:31 -0600

    sort follows within dataset; watermelon background

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  helper_method :sort_column, :sort_direction, :toggle_state
+  
+  def sort_column
+    Dataset.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+  
+  def toggle_state
+    %w[yes no].include?(params[:showAll]) ? params[:showAll] : "no"
+  end
 end
